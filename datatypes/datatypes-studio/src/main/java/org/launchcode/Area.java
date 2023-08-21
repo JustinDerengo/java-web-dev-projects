cda08cd3cb54236c2520712d89834baf59809c66
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-08-21 13:58:11 -0600

    Studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,7 +1,14 @@
 package org.launchcode;
+import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
 
+    System.out.println("Please enter the radius of the circle: ");
+    Double radius = scnr.nextDouble();
+    Double area = Circle.getArea(radius);
+
+    System.out.println("The area of a circle of radius " + radius + "is: " + area);
     }
 }
