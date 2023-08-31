afc748557950cbead7625a5d0ca7273076aad1ad
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-08-31 09:26:04 -0600

    Examples worked

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,6 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void bracketsWithCharReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[c]"));
+    }
+    @Test
+    public void stringWithLeadingSpacesReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("  [boat]"));
+    }
+    @Test
+    public void stringWithTrailingSpacesReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[boat]]    "));
+    }
+    @Test
+    public void stringWithLeadingCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Sail[boat]"));
+    }
+    @Test
+    public void stringWithTrailingCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Sail]boat"));
+    }
+    @Test
+    public void imbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+    @Test
+    public void sequentialBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+    @Test
+    public void tooManyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]"));
+    }
+    @Test
+    public void nonSequentialBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]["));
+    }
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
